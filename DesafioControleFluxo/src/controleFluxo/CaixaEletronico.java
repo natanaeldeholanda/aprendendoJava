177e030ed1e5ed15f02f5e353b00049bd8cd52bf
natanaeldeholanda <natan.holanda@gmail.com>
2023-10-15 20:08:00 -0300

    Diante do conteúdo do curso da DIO, fiz esse código para calcular a média de acordo com os valores informados.

---
+++ DesafioControleFluxo/src/controleFluxo/CaixaEletronico.java
@@ -0,0 +1,19 @@
+package controleFluxo;
+
+public class CaixaEletronico {
+
+	public static void main(String[] args) {
+	double saldo = 25.0;
+	double valorSolicitado = 17.0;
+	
+	if(valorSolicitado <= saldo) {
+		saldo = saldo - valorSolicitado;
+	}else {
+		System.out.println("Saldo insuficiente.");
+	}
+	
+	System.out.println("Seu saldo é: " + "R$" + saldo);
+
+	}
+
+}
