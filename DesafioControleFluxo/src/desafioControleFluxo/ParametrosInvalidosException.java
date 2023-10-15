1b09c564a46bbcbd4c2ff49f0f24804ccc190c25
natanaeldeholanda <natan.holanda@gmail.com>
2023-10-14 23:50:28 -0300

    Resposta do Desafio  Controle de Fluxo

---
+++ DesafioControleFluxo/src/desafioControleFluxo/ParametrosInvalidosException.java
@@ -0,0 +1,12 @@
+package desafioControleFluxo;
+public class ParametrosInvalidosException extends Exception {
+	
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ParametrosInvalidosException(String message) {
+        super(message);
+    }
+}
